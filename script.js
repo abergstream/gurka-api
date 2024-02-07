73595eca3bda300738da85ccf3af404bf7ce6533
Andreas Bergström <andreas@textalkgroup.com>
2024-02-07 22:48:05 +0100

    Added a bit of graphics

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ async function fetchResult() {
     const response = await fetch(url);
     const data = await response.json();
 
+    // Sort by newest game first
+    const newestFirst = data.games.reverse();
     // Loop through each game
-    for (const game of data.games) {
+    for (const game of newestFirst) {
       const { gameID, date } = game;
       const gameDiv = createGameDiv(date);
 
@@ -18,14 +20,22 @@ async function fetchResult() {
       );
 
       // Sort each game by result
-      players.sort((a, b) => b.result - a.result);
+      // players.sort((a, b) => b.result - a.result);
       // Loop through players and append to gameDiv
+      const scoreContainer = createContainer("score-row");
+      const playerContainer = createContainer("players-row");
+
+      let topScore = getMax(players);
+      console.log(topScore);
       players.forEach((player) => {
         const playerName = getPlayerName(data.users, player.userID);
-        const playerDiv = createPlayerDiv(playerName, player.result);
-        gameDiv.appendChild(playerDiv);
+        const playerDiv = createPlayerDiv(playerName);
+        const scoreDiv = createScoreDiv(player.result, topScore);
+        scoreContainer.appendChild(scoreDiv);
+        playerContainer.appendChild(playerDiv);
       });
-
+      gameDiv.appendChild(scoreContainer);
+      gameDiv.appendChild(playerContainer);
       wrapper.appendChild(gameDiv);
     }
   } catch (error) {
@@ -44,17 +54,57 @@ function createGameDiv(date) {
   gameDiv.appendChild(dateDiv);
   return gameDiv;
 }
+function createScoreDiv(content, scoreRange) {
+  const divContainer = document.createElement("div");
+  const scoreDiv = document.createElement("div");
+  // console.log(scoreRange);
+  const percentPerScore = 100 / scoreRange;
+  let score = content;
+  let cssClass = "score-bar--plus";
+  if (score < 0) {
+    cssClass = "score-bar--minus";
+    score = score * -1;
+  }
 
+  const heightPercentage = (percentPerScore * parseFloat(score)) / 2;
+  scoreDiv.style.height = heightPercentage + "%";
+  scoreDiv.classList.add("score-bar", cssClass);
+  scoreDiv.innerText = content;
+  divContainer.appendChild(scoreDiv);
+  divContainer.classList.add("score-container");
+  return divContainer;
+}
 function getPlayerName(users, userID) {
   const user = users.find((user) => user.userID === userID);
   return user ? user.name : "Unknown Player";
 }
-
-function createPlayerDiv(name, result) {
+function createContainer(cssClass) {
+  const container = document.createElement("div");
+  container.classList.add(cssClass);
+  return container;
+}
+function createPlayerDiv(name) {
   const playerDiv = document.createElement("div");
-  playerDiv.textContent = `${name} (${result})`;
+  playerDiv.textContent = `${name}`;
   playerDiv.classList.add("player");
   return playerDiv;
 }
-
+function getMax(players) {
+  let topScore = [];
+  players.forEach((score) => {
+    topScore.push(parseFloat(score.result));
+  });
+  topScore.sort((a, b) => a - b);
+  let lowest = topScore[0] * -1;
+  let highest = topScore[topScore.length - 1];
+  let numberRange = lowest;
+  if (highest > lowest) {
+    numberRange = highest;
+  }
+  if (numberRange % 5 != 0) {
+    return parseFloat(numberRange) + 5 - (numberRange % 5);
+  } else {
+    return parseFloat(numberRange);
+  }
+}
 fetchResult();
